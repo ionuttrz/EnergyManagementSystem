6ab6d0d6e8220e1dacd6fcc97e9c347439207f7e
ionuttrz <Tirziu.Pe.Cristian@student.utcluj.ro>
2024-10-24 13:00:36 +0300

    Backend Microservice for Users

---
+++ EMS_BE_User_Microservice/src/main/java/com/SistemeInformaticeDistribuite/energyManagementSystem/model/ERole.java
@@ -0,0 +1,6 @@
+package com.SistemeInformaticeDistribuite.energyManagementSystem.model;
+
+public enum ERole {
+    CLIENT,
+    ADMIN
+}
---
+++ EMS_BE_User_Microservice/src/main/java/com/SistemeInformaticeDistribuite/energyManagementSystem/model/Role.java
@@ -0,0 +1,20 @@
+package com.SistemeInformaticeDistribuite.energyManagementSystem.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    private ERole role;
+}
