8456d6051a879220c636fc91de8328875314cfd3
ionuttrz <Tirziu.Pe.Cristian@student.utcluj.ro>
2024-10-24 13:09:22 +0300

    Backend Microservice for Devices

---
+++ 2. EMS_BE_Device_Microservice/src/main/java/com/SistemeInformaticeDistribuite/energyManagementSystem/EnergyManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.SistemeInformaticeDistribuite.energyManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyManagementSystemApplication.class, args);
+	}
+
+}
